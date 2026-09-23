d4ef4dd710ce59b7b8a80e9c18fdb0ebd8604eb7
pinqy <19714467+pinqy@users.noreply.github.com>
2026-09-22 22:32:55 -0400

    Create basic express app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+app.use(express.json());
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const allowedOrigins = ['https://aidanhonan.com', 'https://test.aidanhonan.com', 'http://localhost:4200'];
+
+app.use(cors({
+	origin: allowedOrigins,
+}));
+
+app.get('/', (req, res) => {
+	res.json('Hello, world! NodeJS \n');
+});
+
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}`);
+});
