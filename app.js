a3f432f9663387f99b2392674658cc64ec077d01
pinqy <19714467+pinqy@users.noreply.github.com>
2026-09-23 00:30:06 -0400

    Setup server auto-restart on deploy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 
 app.get('/', (req, res) => {
-  res.json('Hello, world! NodeJS -- autodeployed');
+  res.json('Hello, world! NodeJS -- autodeployed2');
 });
 
 app.listen(port, hostname, () => {
