a756aceb72c84f91ba4e55cfe027ed31e6663f51
pinqy <19714467+pinqy@users.noreply.github.com>
2026-09-22 23:59:12 -0400

    Enable pipeline deployment to Beta

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 
 app.get('/', (req, res) => {
-  res.json('Hello, world! NodeJS \n');
+  res.json('Hello, world! NodeJS -- autodeployed');
 });
 
 app.listen(port, hostname, () => {
