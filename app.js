d39e021fd239070d041c1e5cca902bf4fa10d5c3
pinqy <19714467+pinqy@users.noreply.github.com>
2026-09-22 23:16:18 -0400

    Setup ESLint + add PR check action

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 
@@ -9,13 +9,13 @@ const port = 3000;
 const allowedOrigins = ['https://aidanhonan.com', 'https://test.aidanhonan.com', 'http://localhost:4200'];
 
 app.use(cors({
-	origin: allowedOrigins,
+  origin: allowedOrigins,
 }));
 
 app.get('/', (req, res) => {
-	res.json('Hello, world! NodeJS \n');
+  res.json('Hello, world! NodeJS \n');
 });
 
 app.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
